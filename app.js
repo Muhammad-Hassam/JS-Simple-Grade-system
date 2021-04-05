d2663cb1311415781dcf55e7dcdc4240d26d82af
Muhammad Hassam Shaikh <hassamshaikh94@gmail.com>
2021-04-05 11:43:23 +0500

    grade system

---
+++ app.js
@@ -0,0 +1,22 @@
+var num=prompt("Enter numbers");
+if(num<=100 && num>=90){
+    document.write("You Got A+ Grade");
+}
+else if(num<=89 && num>=80){
+    document.write("You Got A Grade");
+}
+else if(num<=79 && num>=70){
+    document.write("You Got B Grade");
+}
+else if(num<=69 && num>=60){
+    document.write("You Got C Grade");
+}
+else if(num<=59 && num>=50){
+    document.write("You Got D Grade");
+}
+else if(num<=49 && num>=40){
+    document.write("You Got E Grade");
+}
+else if(num<=39 && num>=0){
+    document.write("You Got F Grade");
+}
